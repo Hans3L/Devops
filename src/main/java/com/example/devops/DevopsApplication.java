371a7506efac6c4dd57e73f1a3c04b0cc2efc8e9
hansel <hansel.amilcar.ingaruca.rimac@everis.com>
2020-02-23 15:32:29 -0500

    remove target

--- src/main/java/com/example/devops/DevopsApplication.java
+++ src/main/java/com/example/devops/DevopsApplication.java
@@ -3,6 +3,7 @@ package com.example.devops;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@lombok.Generated
 @SpringBootApplication
 public class DevopsApplication {
 
--- src/main/java/com/example/devops/web/WelcomeController.java
+++ src/main/java/com/example/devops/web/WelcomeController.java
@@ -5,9 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 public class WelcomeController {
-
+    @lombok.Generated
     @RequestMapping("/")
     public String welcome(Model model){
         model.addAttribute("course","DevOps Senior");
